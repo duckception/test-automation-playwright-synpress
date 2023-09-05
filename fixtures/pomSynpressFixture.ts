35ff43ba22e67bea48caec3156a7529ef3c52137
DavitMkhitaryan <davitmkhitaryanit@gmail.com>
2023-09-05 21:59:50 +0400

    Github workflow test 3

--- fixtures/pomSynpressFixture.ts
+++ fixtures/pomSynpressFixture.ts
@@ -1,6 +1,8 @@
 import { test as base, chromium, type BrowserContext } from '@playwright/test';
 import { initialSetup } from '@synthetixio/synpress/commands/metamask';
 import { prepareMetamask } from '@synthetixio/synpress/helpers';
+import { setExpectInstance } from '@synthetixio/synpress/commands/playwright';
+import { resetState } from '@synthetixio/synpress/commands/synpress';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -8,14 +10,29 @@ interface pagesAndContext {
   context: BrowserContext;
 }
 
-const privateKey: string = process.env.METAMASK_SETUP_PRIVATE_KEY!;
-const network: string = process.env.METAMASK_SETUP_NETWORK!;
-const password: string = process.env.METAMASK_SETUP_PASSWORD!;
+let privateKey: string;
+let network: string;
+let password: string;
+
+if (
+  process.env.METAMASK_SETUP_PRIVATE_KEY != null &&
+  process.env.METAMASK_SETUP_NETWORK != null &&
+  process.env.METAMASK_SETUP_PASSWORD != null
+) {
+  privateKey = process.env.METAMASK_SETUP_PRIVATE_KEY;
+  network = process.env.METAMASK_SETUP_NETWORK;
+  password = process.env.METAMASK_SETUP_PASSWORD;
+} else {
+  throw new Error(
+    'One or more of required Metamask setup environment variables is not set',
+  );
+}
 
 export const test = base.extend<pagesAndContext>({
+  // eslint-disable-next-line no-empty-pattern
   context: async ({}, use) => {
-    // required for synpress
-    global.expect = expect;
+    // required for synpress as it shares same expect instance as playwright
+    await setExpectInstance(expect);
     // download metamask
     const metamaskPath: string = await prepareMetamask(
       process.env.METAMASK_VERSION != null || '10.25.0',
@@ -29,12 +46,12 @@ export const test = base.extend<pagesAndContext>({
     if (process.env.CI != null) {
       browserArgs.push('--disable-gpu');
     }
-    if (process.env.METAMASK_SETUP_HEADLESS_MODE === 'true') {
+    if (process.env.HEADLESS_MODE === 'true') {
       browserArgs.push('--headless=new');
     }
     // launch browser
     const context = await chromium.launchPersistentContext('', {
-      headless: true,
+      headless: false,
       args: browserArgs,
     });
     // wait for metamask
@@ -49,7 +66,8 @@ export const test = base.extend<pagesAndContext>({
     });
     await use(context);
     await context.close();
-  }
+    await resetState();
+  },
 });
 
 export const expect = test.expect;
