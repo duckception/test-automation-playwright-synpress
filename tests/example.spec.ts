7d8d9bd95b5e72206d9eab77810079a42e5a63e9
duckception <spidiritsu@gmail.com>
2023-09-06 23:42:07 +0200

    Trigger CI

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,7 +7,7 @@ test.describe('Dummy Tests', async () => {
   // Network correctly switches to Polygon only here
   test('Test 1', async ({ page }) => {
     const currentNetwork = getCurrentNetwork();
-    console.log('Test 1', {id: currentNetwork.id, name: currentNetwork.name, network: currentNetwork.network})
+    console.log('Test 1', { id: currentNetwork.id, name: currentNetwork.name, network: currentNetwork.network })
     await page.goto('https://dapp-citizen-react.vercel.app');
     await page.getByText('Connect').click();
     await metamask.acceptAccess();
@@ -18,7 +18,7 @@ test.describe('Dummy Tests', async () => {
   // Starting from this test network will not switch to polygon again. Please note that getCurrentNetwork() shows Polygon, but in reality Polygon is not even added
   test('Test 2', async ({ page }) => {
     const currentNetwork = getCurrentNetwork();
-    console.log('Test 2', {id: currentNetwork.id, name: currentNetwork.name, network: currentNetwork.network})
+    console.log('Test 2', { id: currentNetwork.id, name: currentNetwork.name, network: currentNetwork.network })
     await page.goto('https://dapp-citizen-react.vercel.app');
     await page.getByText('Connect').click();
     await metamask.acceptAccess();
@@ -27,7 +27,7 @@ test.describe('Dummy Tests', async () => {
 
   test('Test 3', async ({ page }) => {
     const currentNetwork = getCurrentNetwork();
-    console.log('Test 3', {id: currentNetwork.id, name: currentNetwork.name, network: currentNetwork.network})
+    console.log('Test 3', { id: currentNetwork.id, name: currentNetwork.name, network: currentNetwork.network })
     await page.goto('https://dapp-citizen-react.vercel.app');
     await page.getByText('Connect').click();
     await metamask.acceptAccess();
@@ -36,7 +36,7 @@ test.describe('Dummy Tests', async () => {
 
   test('Test 4', async ({ page }) => {
     const currentNetwork = getCurrentNetwork();
-    console.log('Test 4', {id: currentNetwork.id, name: currentNetwork.name, network: currentNetwork.network})
+    console.log('Test 4', { id: currentNetwork.id, name: currentNetwork.name, network: currentNetwork.network })
     await page.goto('https://dapp-citizen-react.vercel.app');
     await page.getByText('Connect').click();
     await metamask.acceptAccess();
